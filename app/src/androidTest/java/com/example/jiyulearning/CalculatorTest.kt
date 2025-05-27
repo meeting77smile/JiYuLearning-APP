d847fbe45c94c3525ed1ca2c95639ba3cdc36760
Limpid <Limpid8818@outlook.com>
2025-05-27 10:17:39 +0800

    First Test

---
+++ app/src/androidTest/java/com/example/jiyulearning/CalculatorTest.kt
@@ -0,0 +1,28 @@
+package com.example.jiyulearning
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CalculatorTest {
+    @get: Rule
+    val activityRule = ActivityScenarioRule(CalculatorActivity::class.java)
+
+    @Test
+    fun plusTest() {
+        onView(withId(R.id.btn_one)).perform(click())
+        onView(withId(R.id.btn_plus)).perform(click())
+        onView(withId(R.id.btn_two)).perform(click())
+        onView(withId(R.id.btn_equal)).perform(click())
+
+        onView(withId(R.id.tv_result)).check(matches(withText("1+2=3.0")))
+    }
+}
